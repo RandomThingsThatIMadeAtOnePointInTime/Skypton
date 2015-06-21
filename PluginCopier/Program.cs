5e0a606322a24ecbb3ef1be02089f61899e26109
Scarsz <Scarsz@users.noreply.github.com>
2015-06-20 17:45:31 -0700

    Much cleaner plugin copying system

---
+++ PluginCopier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PluginCopier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Skypton\bin\Debug");
+            Directory.SetCurrentDirectory(destination);
+
+            List<string> files = new List<string>();
+            foreach (string file in Directory.EnumerateFiles(@"..\..\..", "Plugin_*.dll", SearchOption.AllDirectories))
+                if (!files.Contains(file.Split('\\').Last()))
+                    if (file.Contains("bin"))
+                        if (!file.Contains("Plugins"))
+                            files.Add(file);
+            
+            Console.WriteLine("\nCopying plugins to build folder...");
+            Directory.CreateDirectory("Plugins");
+            foreach (string plugin in files)
+            {
+                Console.WriteLine("Coping " + plugin + " -> Plugins\\" + plugin.Split('\\').Last());
+                File.Copy(plugin, destination + "\\Plugins\\" + plugin.Split('\\').Last(), true);
+            }
+            Console.WriteLine();
+        }
+    }
+}

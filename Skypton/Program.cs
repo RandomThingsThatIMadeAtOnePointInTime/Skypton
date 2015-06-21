4c18460d4d9cc6c17a8b32ea8bd74b21dba5f5d9
Scarsz <Scarsz@users.noreply.github.com>
2015-06-20 19:34:20 -0700

    Better using placements

--- Skypton/Program.cs
+++ Skypton/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using INIHandler;
+﻿using INIHandler;
 using PluginHandler;
 using SKYPE4COMLib;
-using System.Reflection;
+
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading;
 
